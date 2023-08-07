fac87db833a54cd239ba155cd841700818d14953
Pascal Bustamante <pascalbustamante46@gmail.com>
2023-08-07 12:05:45 -0500

    start to JWT

--- backend/auth/auth.py
+++ backend/auth/auth.py
@@ -1,28 +1,29 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for, session
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for, session,flash
 from database.models import User, db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user, current_user
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 
 auth = Blueprint('auth', __name__, template_folder='templates', static_folder='static')
 
+jwt = JWTManager()
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
+        email = request.form.get('email', None)
+        password = request.form.get('password', None)
 
         user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
-                flash('Logged in successfully!', category='success')
-                login_user(user, remember=True)
-                return redirect(url_for('views.home'))
+                access_token = create_access_token(identity=email)
+                return jsonify(access_token=access_token)
             else:
-                flash('Incorrect password, try again.', category='error')
+                return jsonify({'msg': 'Incorrect email or password, try again.'}), 401
         else:
-            flash('Email does not exist.', category='error')
+            return jsonify({'msg': 'Incorrect email or password, try again.'}), 401
 
     return render_template("login.html", user=current_user)
 
--- backend/create_app.py
+++ backend/create_app.py
@@ -12,18 +12,22 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config["JWT_SECRET_KEY"] = "super-secret" 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:BraianNico11@localhost/PetApp'
     db.init_app(app)
 
+
     from general.views import views
-    from auth.auth import auth
+    from auth.auth import auth, jwt 
     from api.api import api_petfinder
     from database.views import database
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/auth')
+    jwt.init_app(app)
     app.register_blueprint(database, url_prefix='/')
     #app.register_blueprint(api_petfinder, url_prefix='/')
+
 
     with app.app_context():
         db.create_all()
