2b95c183ae024e21e2becac83faeb9827ebc1ee6
Pascal Bustamante <pascalbustamante46@gmail.com>
2023-08-07 19:08:38 -0500

    typo with app component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
 import ListGroup from "./components/ListGroup";
 import { Routes, Route } from "react-router-dom";
 
-function App() {
-  return <Routes>
+const App = () =>  {
+  return(
+  <>
+  <nav>
+    <ul>
+      <li></li>
+    </ul>
+  </nav>
+  <Routes>
     <Route path="/" element={<h1>HI</h1>} />
     <Route />
   </Routes>
+  </>
+  )
 }
 export default App;
